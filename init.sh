af9b3edfbf1a45b1137a88f9c7c4859821dbd3c2
fibek <igorfib6@gmail.com>
2023-04-01 22:04:14 +0200

    update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,16 @@
-#!/bin/sh 
+#!/bin/sh
 
-python3 tempcontroller.py &
-uvicorn server:app --host 0.0.0.0 --port 8000 
+# /etc/init.d/init.sh
+### BEGIN INIT INFO
+# Provides:          init.sh
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description:
+# Description:       Start temperature controller and its server
+### END INIT INFO
+
+(cd ~/TemperatureController ; python3 tempcontroller.py 2>&1 > /dev/null &)
+(cd ~/TemperatureController ; uvicorn server:app --host 0.0.0.0 --port 8000 2>&1 > /dev/null  &)
+
